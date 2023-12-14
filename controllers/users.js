a409718995b3d900121b350d16a0c32785252e72
DezmonDND <denedoseikin@mail.ru>
2023-12-14 20:06:46 +0700

    feat: вынести логику обновленяи данных пользвателя

--- controllers/users.js
+++ controllers/users.js
@@ -64,45 +64,18 @@ const findUserById = (req, res, userData, next) => {
 
 module.exports.getUsersMe = (req, res, next) => {
     const userData = req.user._id;
-    // User.findById(req.user._id)
-    //     .then((user) => {
-    //         if (!user) {
-    //             next(new NotFoundError('Пользователь по указанному _id не найден.'));
-    //         } else {
-    //             res.status(200).send({ data: user });
-    //         }
-    //     })
     findUserById(req, res, userData, next);
-        // .catch((err) => {
-        //     return next(err);
-        // });
 }
 
 module.exports.getOneUser = (req, res, next) => {
     const userData = req.params.userId;
-    // User.findById(req.params.userId)
-    //     .then((user) => {
-    //         if (!user) {
-    //             next(new NotFoundError('Пользователь по указанному _id не найден.'));
-    //         } else {
-    //             res.status(200).send({ data: user });
-    //         }
-    //     })
     findUserById(req, res, userData, next);
-        // .catch((err) => {
-        //     if (err instanceof mongoose.Error.CastError) {
-        //         next(new BadRequestError('Переданы некорректные данные при поиске пользователя.'));
-        //     } else {
-        //         next(err);
-        //     }
-        // });
 }
 
-module.exports.updateUsersData = (req, res, next) => {
-    const { name, about } = req.body;
+const updateData = (req, res, dataForUpdate, next) => {
     User.findByIdAndUpdate(
         req.user._id,
-        { name, about },
+        { dataForUpdate },
         {
             new: true,
             runValidators: true
@@ -124,30 +97,58 @@ module.exports.updateUsersData = (req, res, next) => {
         });
 }
 
+module.exports.updateUsersData = (req, res, next) => {
+    const { name, about } = req.body;
+    updateData(req, res, name, about, next);
+    // User.findByIdAndUpdate(
+    //     req.user._id,
+    //     { name, about },
+    //     {
+    //         new: true,
+    //         runValidators: true
+    //     }
+    // )
+    //     .then((user) => {
+    //         if (!user) {
+    //             next(new NotFoundError('Пользователь по указанному _id не найден.'))
+    //         } else {
+    //             res.status(200).send({ data: user });
+    //         }
+    //     })
+    //     .catch((err) => {
+    //         if (err instanceof mongoose.Error.ValidationError) {
+    //             next(new BadRequestError('Переданы некорректные данные при обновлении данных пользователя.'))
+    //         } else {
+    //             next(err);
+    //         }
+    //     });
+}
+
 module.exports.updateAvatar = (req, res, next) => {
     const { avatar } = req.body;
-    return User.findByIdAndUpdate(
-        req.user._id,
-        { avatar },
-        {
-            new: true,
-            runValidators: true
-        }
-    )
-        .then((user) => {
-            if (!user) {
-                next(new NotFoundError('Пользователь по указанному _id не найден.'))
-            } else {
-                res.status(200).send({ data: user });
-            }
-        })
-        .catch((err) => {
-            if (err instanceof mongoose.Error.ValidationError) {
-                next(new BadRequestError('Переданы некорректные данные при обновлении аватара пользователя.'))
-            } else {
-                next(err);
-            }
-        });
+    updateData(req, res, avatar, next);
+    // return User.findByIdAndUpdate(
+    //     req.user._id,
+    //     { avatar },
+    //     {
+    //         new: true,
+    //         runValidators: true
+    //     }
+    // )
+    //     .then((user) => {
+    //         if (!user) {
+    //             next(new NotFoundError('Пользователь по указанному _id не найден.'))
+    //         } else {
+    //             res.status(200).send({ data: user });
+    //         }
+    //     })
+    //     .catch((err) => {
+    //         if (err instanceof mongoose.Error.ValidationError) {
+    //             next(new BadRequestError('Переданы некорректные данные при обновлении аватара пользователя.'))
+    //         } else {
+    //             next(err);
+    //         }
+    //     });
 }
 
 module.exports.login = (req, res, next) => {

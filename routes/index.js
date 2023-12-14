639ffadcd4b98a6010678c7d9739393e2e39a88f
DezmonDND <denedoseikin@mail.ru>
2023-12-14 18:54:07 +0700

    fix: исправить пути

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const routes = require('express').Router();
 const cardsRouter = require("./cards");
 const usersRouter = require("./users");
-const auth = require('./middlewares/auth');
-const { createUser, login } = require('./controllers/users');
-const { loginValidation, createUserValidation } = require('./middlewares/validationJoi');
+const auth = require('../middlewares/auth');
+const { createUser, login } = require('../controllers/users');
+const { loginValidation, createUserValidation } = require('../middlewares/validationJoi');
 
 
 app.post('/signin', loginValidation, login);

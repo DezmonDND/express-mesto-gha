bc5c9feaae9f2bb9a81e8e897de014f0bfcb8f7e
DezmonDND <denedoseikin@mail.ru>
2023-12-18 13:26:33 +0700

    fix: исправить замечания после ревью

--- controllers/cards.js
+++ controllers/cards.js
@@ -36,7 +36,7 @@ module.exports.deleteCard = (req, res, next) => {
             if (userId !== card.owner.toString()) {
                 next(new ForbiddenError('Невозможно удалить карточку.'));
             }
-            return cardId.deleteOne()
+            return card.deleteOne()
                 .then(() => res.status(200).send({ message: 'Карточка удалена' }));
         })
         .catch((err) => {
